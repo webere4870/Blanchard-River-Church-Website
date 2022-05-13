5afa17b140b3cf7aa314d65563cd49fe927b9f8a
Eli Weber <eliweber2001@gmail.com>
2022-05-13 17:20:19 -0400

    Navigation changes. Contact strip

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
-doubler = lambda x: x + 10
+from multiprocessing import Process, cpu_count
+import string
+import time
 
-print(doubler(10))
-print(doubler(20))
+def counter(num):
+    count =0
+    while count < num:
+        count +=1 
+
+def main():
+    print(cpu_count)
+    a = Process(target=counter, args=(100000000, ))
+    b = Process(target=counter, args=(50000000, ))
+    a.start()
+    a.join()
+    print("Finished in: " + str(time.perf_counter()))
+
+
+if __name__ == "__main__":
+    main()
