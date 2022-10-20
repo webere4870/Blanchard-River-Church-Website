8202737abfda6011d312d186e51a722dd58f0c7f
Eli Weber <eliweber2001@gmail.com>
2022-10-20 15:04:52 -0400

    History page css mods

--- Partials.js
+++ Partials.js
@@ -1,4 +1,23 @@
-let nav=``
+let nav=`
+<nav>
+        <a href="./index.html">Home</a>
+        <a href="./history.html">History</a>
+        <a href="./doctrine.html">Doctrine</a>
+        <a href="./info.html">Info</a>
+        <a href="./contact.html">Contact</a>
+</nav>
+
+<input type="checkbox" id="active">
+<label for="active" class="menu-btn"><i style="color: black" class="fas fa-bars"></i></label>
+<div class="wrapper">
+    <ul>
+        <li><a href="./index.html">Home</a></li>
+        <li><a href="./history.html">History</a></li>
+        <li><a href="./doctrine.html">Doctrine</a></li>
+        <li><a href="./info.html">Info</a></li>
+        <li><a href="./contact.html">Contact</a></li>
+    </ul>
+</div> `
 
 
 export {nav}
