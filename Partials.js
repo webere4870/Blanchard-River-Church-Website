a0b8b2f7f02e55d680c10f3b8495b136bd5a7793
Eli Weber <eliweber2001@gmail.com>
2022-12-14 19:22:23 -0500

    Stripe checkout

--- Partials.js
+++ Partials.js
@@ -5,6 +5,7 @@ let nav=`
 <a href="./about.html" class="swipeInDude">About</a>
 <a href="./history.html" class="swipeInDude">History</a>
 <a href="./live.html" class="swipeInDude">Watch Live</a>
+<a href="https://buy.stripe.com/14k2aV4Vt6DC944bII" target="_blank" class="swipeInDude">Donate</a>
 </nav>
 
 <input type="checkbox" id="active">
@@ -16,6 +17,7 @@ let nav=`
     <li><a href="./about.html" class="swipeInDude">About</a><li/>
     <li><a href="./history.html" class="swipeInDude">History</a><li/>
     <li><a href="./live.html" class="swipeInDude">Watch Live</a><li/>
+    <li><a href="https://buy.stripe.com/14k2aV4Vt6DC944bII" class="swipeInDude" target="_blank">Donate</a><li/>
     </ul>
 </div> `
 
