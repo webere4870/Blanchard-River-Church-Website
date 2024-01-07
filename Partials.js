0a36eb385648ebbb09a6773b79f6f7bb572a866f
Eli Weber <eliweber2001@gmail.com>
2024-01-06 23:00:36 -0500

    almost done

--- Partials.js
+++ Partials.js
@@ -4,21 +4,47 @@ let nav=`
 <a href="./calendar.html" class="swipeInDude">Calendar</a>
 <a href="./about.html" class="swipeInDude">About</a>
 <a href="./history.html" class="swipeInDude">History</a>
+<a href="./newsletter.html" class="swipeInDude">Newsletter</a>
 <a href="./live.html" class="swipeInDude">Watch Live</a>
-<a href="https://buy.stripe.com/14k2aV4Vt6DC944bII" target="_blank" class="swipeInDude">Donate</a>
 </nav>
 
-<input type="checkbox" id="active">
-<label for="active" class="menu-btn"><i style="color: black" class="fas fa-bars"></i></label>
 <div class="wrapper">
-    <ul>
-    <li><a href="./index.html" class="swipeInDude">Home</a><li/>
-    <li><a href="./calendar.html" class="swipeInDude">Calendar</a><li/>
-    <li><a href="./about.html" class="swipeInDude">About</a><li/>
-    <li><a href="./history.html" class="swipeInDude">History</a><li/>
-    <li><a href="./live.html" class="swipeInDude">Watch Live</a><li/>
-    <li><a href="https://buy.stripe.com/14k2aV4Vt6DC944bII" class="swipeInDude" target="_blank">Donate</a><li/>
+<nav class="navbar navbar-expand-lg bg-light">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          Dropdown
+        </a>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" href="#">Action</a></li>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Something else here</a></li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
     </ul>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</div>
+</nav>
 </div> `
 
 
