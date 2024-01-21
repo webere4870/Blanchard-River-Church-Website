da5d60c3701971f5a05683f58d87ff22d9a7b35d
Eli Weber <eliweber2001@gmail.com>
2024-01-21 12:54:29 -0500

    Done

--- LoadPartials.js
+++ LoadPartials.js
@@ -2,4 +2,38 @@ import {nav} from './Partials.js'
 $().ready(()=>
 {
     $( "body" ).append(nav)
+    setInterval(()=>
+    {
+        if(document.querySelectorAll(".sticky")[0]?.offsetHeight == 80 && window.innerWidth <= 768)
+        {
+            $("nav a").css("opacity", "0", 200)
+        }
+    },300)
+
+    window.addEventListener("resize", (evt)=>
+    {
+        console.log("rezi")
+        if(window.innerWidth > 768)
+        {
+            $("nav a").css("opacity", "1")
+            $(".sticky").animate({height: "80px"})
+        }
+        else{
+            $("nav a").css("opacity", "0")
+           
+        }
+    })
+    
+    $("body").on("click", ".hamburgery", (evt) => {
+        if(document.querySelectorAll(".sticky")[0].offsetHeight == 80)
+        {
+            $(".sticky").animate({height: "100vh", color: "black"}, 200);
+            $("nav a").css("opacity", "1", 200)
+        }
+        else
+        {
+            $("nav a").css("opacity", "0", 200)
+            $(".sticky").animate({height: "80px", color: "black"}, 200);
+        }
+    });
 })
--- control.js
+++ control.js
@@ -18,7 +18,7 @@ $('.navBox').click((evt)=>
 
 setInterval(()=>
 {
-  if(window.scrollY > 0 || location.href.includes('about') || location.href.includes('live'))
+  if(window.scrollY > 0 || location.href.includes('about') || location.href.includes('live') || window.innerWidth <= 768)
   {
     $('nav').attr("class", "sticky")
   }
@@ -28,15 +28,17 @@ setInterval(()=>
   }
 }, 200)
 
-let latitude = 41.037660;
-    let longitude = -83.649540;
+
+
+let latitude = 40.782250;
+    let longitude = -83.581690;
     let mymap = L.map('mapid', {tap: false}).setView([latitude, longitude], 13);
     const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">Open Street Map</a> contributors';
     const tileURL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
     const tiles = L.tileLayer(tileURL, {attribution: attribution});
     tiles.addTo(mymap);
     var marker = L.marker([latitude, longitude]).addTo(mymap);
-    marker.bindPopup("113 East Crawford Street, Findlay, OH 45840");
+    marker.bindPopup("15745 OH-81, Dunkirk, OH 45836");
     marker.on("click", function (event) {
         event.target.openPopup();
      });
